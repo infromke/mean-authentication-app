c8fc72803c9f251a802eb26f7b65c9e5c8202a33
Inaros Lemuel <infromke@gmail.com>
2026-05-20 18:39:05 -0300

    refactor: migrate throwHttpError utility to TypeScript

--- backend/src/utils/throwHttpError.ts
+++ backend/src/utils/throwHttpError.ts
@@ -4,12 +4,10 @@
  * @param {string} message - A mensagem de erro. Mapeia para `detail` na RFC.
  * @param {Array} invalidParams - (Opcional) Array de erros formatados do Zod. Mapeia para `errors`.
  */
-const throwHttpError = (status, message, invalidParams = null) => {
-  const error = new Error(message)
+const throwHttpError = (status: number, message: string, invalidParams?: any[]) => {
+  const error = new Error(message) as any
   error.status = status
-
-  if (invalidParams) error.errors = invalidParams // para o array de erros do Zod
-
+  error.errors = invalidParams // para o array de erros do Zod
   throw error
 }
 
