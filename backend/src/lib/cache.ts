901ffce174dc6eb494dd9e3dbf02d963c43b11c5
Inaros Lemuel <infromke@gmail.com>
2026-05-24 01:23:46 -0300

    docs: improve in-memory cache JSDoc description

--- backend/src/lib/cache.ts
+++ backend/src/lib/cache.ts
@@ -1,8 +1,9 @@
 import NodeCache from 'node-cache'
 
 /**
- * Instância do cache. `stdTTL` é o tempo padrão de vida (5 minutos) e `checkperiod`
- * é o tempo para deletar itens expirados (2 minutos).
+ * Instância do cache na memória.
+ * `stdTTL` é o tempo padrão de vida (5 minutos) e
+ * `checkperiod` é o tempo de varredura para deletar itens expirados (2 minutos).
  */
 const cache = new NodeCache({ stdTTL: 300, checkperiod: 120 })
 
