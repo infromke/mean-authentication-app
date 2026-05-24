4865eaa12f8955a935b2706f9a7fc9c0bccabc3b
Inaros Lemuel <infromke@gmail.com>
2026-05-24 03:19:20 -0300

    refactor: migrate CORS configuration to TypeScript
    - apply "CorsOptions" type definition to the middleware configuration object

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,11 +1,11 @@
-import cors from 'cors'
+import cors, { type CorsOptions } from 'cors'
 
 const allowedOrigins = [
   process.env.CLIENT_PORT ? `http://localhost:${process.env.CLIENT_PORT}` : null,
   process.env.FRONTEND_URL || null, // o render irá injetar essa variável
 ].filter(Boolean) // filtra apenas os valores que não são "undefined"
 
-const corsOptions = {
+const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true)
