e791c226adf6253dc22f6fe3cb3307e0add21802
Inaros Lemuel <infromke@gmail.com>
2026-05-24 00:45:52 -0300

    refactor: rename "password.ts" helper to "hash.ts" and migrate it to TypeScript

--- backend/src/utils/hash.ts
+++ backend/src/utils/hash.ts
@@ -5,10 +5,9 @@ import bcrypt from 'bcrypt'
  * @param {string} password - Senha fornecida.
  * @returns {promise} Senha hasheada.
  */
-const generatePassword = async (password) => {
+const generatePassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
-
   return hash
 }
 
@@ -16,9 +15,9 @@ const generatePassword = async (password) => {
  * Compara senhas de forma assíncrona.
  * @param {string} givenPwd - Senha fornecida.
  * @param {string} storedPwd - Senha armazenada no banco de dados.
- * @returns {promise} true ou false.
+ * @returns {promise} true se as senhas coincidirem, caso contrário false.
  */
-const validatePassword = async (givenPwd, storedPwd) =>
+const validatePassword = async (givenPwd: string, storedPwd: string): Promise<boolean> =>
   await bcrypt.compare(givenPwd, storedPwd)
 
 export { generatePassword, validatePassword }
