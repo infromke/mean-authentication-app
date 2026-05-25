6b501723b3dd5a9442c7be7a57c3b6b0b6546327
Inaros Lemuel <infromke@gmail.com>
2026-05-24 22:38:32 -0300

    refactor: establish explicit "never" return type for throwHttpError util

--- backend/src/utils/throwHttpError.ts
+++ backend/src/utils/throwHttpError.ts
@@ -4,7 +4,7 @@
  * @param {string} message - A mensagem de erro. Mapeia para `detail` na RFC.
  * @param {Array} errors - (Opcional) Array de erros formatados do Zod.
  */
-const throwHttpError = (status: number, message: string, errors?: any[]) => {
+const throwHttpError = (status: number, message: string, errors?: any[]): never => {
   const error = new Error(message) as any
   error.status = status
   error.errors = errors // para o array de erros do Zod
