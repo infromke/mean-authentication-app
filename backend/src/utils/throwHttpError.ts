18dbd96c1afdc1581cd175ad1a51092a79a52aac
Inaros Lemuel <infromke@gmail.com>
2026-05-24 22:37:20 -0300

    style: rename "invalidParams" parameter to "errors"

--- backend/src/utils/throwHttpError.ts
+++ backend/src/utils/throwHttpError.ts
@@ -2,12 +2,12 @@
  * Cria um objeto Error com um status HTTP personalizado. Também lança o erro.
  * @param {number} status - O código de status HTTP a ser retornado.
  * @param {string} message - A mensagem de erro. Mapeia para `detail` na RFC.
- * @param {Array} invalidParams - (Opcional) Array de erros formatados do Zod. Mapeia para `errors`.
+ * @param {Array} errors - (Opcional) Array de erros formatados do Zod.
  */
-const throwHttpError = (status: number, message: string, invalidParams?: any[]) => {
+const throwHttpError = (status: number, message: string, errors?: any[]) => {
   const error = new Error(message) as any
   error.status = status
-  error.errors = invalidParams // para o array de erros do Zod
+  error.errors = errors // para o array de erros do Zod
   throw error
 }
 
