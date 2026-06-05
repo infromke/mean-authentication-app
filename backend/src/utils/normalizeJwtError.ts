1c97771607e502602fc2e7f794a92ca4a9f5e63c
Inaros Lemuel <infromke@gmail.com>
2026-06-05 08:29:49 -0300

    feat: create normalizeJwtError utility for type-safe JWT exceptions

---
+++ backend/src/utils/normalizeJwtError.ts
@@ -0,0 +1,37 @@
+const isEnvDev = process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'development'
+
+interface CustomHttpError extends Error {
+  status?: number
+}
+
+/**
+ * Normaliza erros disparados pela biblioteca jsonwebtoken, adicionando status HTTP
+ * e mensagens semânticas baseadas no ambiente.
+ */
+const normalizeJwtError = (error: unknown): CustomHttpError => {
+  // se já for uma instância de Error (que o jsonwebtoken lança)
+  if (error instanceof Error) {
+    const jwtError = error as CustomHttpError
+
+    // config generalizada
+    jwtError.status = 401
+    jwtError.message = isEnvDev ? 'Invalid token' : 'Access denied'
+
+    // específica para o erro de validade
+    if (error.name === 'TokenExpiredError') {
+      jwtError.status = 403
+      jwtError.message = isEnvDev ? 'Token has expired' : 'Session expired'
+    }
+
+    return jwtError
+  }
+
+  // padrão para erros que não são instância de Error
+  return {
+    name: 'UnknownJwtError',
+    message: isEnvDev ? 'Unknown error while validating token' : 'Access denied',
+    status: 401,
+  }
+}
+
+export default normalizeJwtError
