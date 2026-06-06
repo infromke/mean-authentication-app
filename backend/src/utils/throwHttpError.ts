3aa28a33b4a316a5338edaa9b25b58002dd4c02a
Inaros Lemuel <infromke@gmail.com>
2026-06-05 23:09:47 -0300

    refactor: replace "any[]" with Zod's native type "ZodIssue"
    - create HttpError interface extending the native Error class with status and "errors" array

--- backend/src/utils/throwHttpError.ts
+++ backend/src/utils/throwHttpError.ts
@@ -1,13 +1,24 @@
+import type { ZodIssue } from 'zod'
+
+interface HttpError extends Error {
+  status: number
+  errors?: ZodIssue[] | unknown[]
+}
+
 /**
  * Cria um objeto Error com um status HTTP personalizado. Também lança o erro.
  * @param {number} status - O código de status HTTP a ser retornado.
  * @param {string} message - A mensagem de erro. Mapeia para `detail` na RFC.
  * @param {Array} errors - (Opcional) Array de erros formatados do Zod.
  */
-const throwHttpError = (status: number, message: string, errors?: any[]): never => {
-  const error = new Error(message) as any
+const throwHttpError = (
+  status: number,
+  message: string,
+  errors?: ZodIssue[] | unknown[],
+): never => {
+  const error = new Error(message) as HttpError
   error.status = status
-  error.errors = errors // para o array de erros do Zod
+  if (errors) error.errors = errors // para o array de erros do Zod
   throw error
 }
 
