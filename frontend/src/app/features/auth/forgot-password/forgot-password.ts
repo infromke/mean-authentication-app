26df5e3bc7e7756e80e3c92a3b27c6d246c775b7
Inaros Lemuel <infromke@gmail.com>
2026-06-14 21:06:46 -0300

    feat: add automatic redirection for active password reset sessions
    - implement ngOnInit() hook to silently evaluate existing "passwordToken" cookie on page load
    - handle 409 Conflict server responses when requesting for a password reset a second time

--- frontend/src/app/features/auth/forgot-password/forgot-password.ts
+++ frontend/src/app/features/auth/forgot-password/forgot-password.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -14,7 +14,7 @@ import { InputGroup } from '../../../shared/components/input-group/input-group';
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
 })
-export class ForgotPassword {
+export class ForgotPassword implements OnInit {
   private formBuilder = inject(NonNullableFormBuilder);
   private authService = inject(AuthService);
   private userService = inject(UserService);
@@ -25,6 +25,19 @@ export class ForgotPassword {
   form = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
   });
+
+  // verifica se o usuário já possui uma sessão de redefinição de senha
+  ngOnInit(): void {
+    this.authService.checkResetStatus().subscribe({
+      next: () => {
+        this.router.navigate(['/forgot-password/reset']);
+        this.toastr.info('We resumed where you previously left. You can continue!');
+      },
+      error: (err) => {
+        if (err.status === 401 || err.status === 403) return;
+      },
+    });
+  }
 
   onForgotSubmit(): void {
     if (this.form.invalid) {
@@ -41,6 +54,13 @@ export class ForgotPassword {
         this.toastr.info(res.message);
       },
       error: (err) => {
+        if (err.status === 409) {
+          // caso o usuário já tenha um código ativo para verificação que ainda não expirou
+          this.router.navigate(['/forgot-password/verify']);
+          this.toastr.info(err.error?.detail);
+          return;
+        }
+
         this.toastr.error(err.error?.detail);
       },
     });
